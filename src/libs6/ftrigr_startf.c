61f2b1f260de888ef25a86f35c3c3c4d013100a3
Laurent Bercot <ska-skaware@skarnet.org>
2014-12-09 14:39:11 +0000

    Make ftrigr_startf data static const

--- src/libs6/ftrigr_startf.c
+++ src/libs6/ftrigr_startf.c
@@ -6,7 +6,7 @@
 
 int ftrigr_startf (ftrigr_ref a, tain_t const *deadline, tain_t *stamp)
 {
-  char const *cargv[2] = { FTRIGRD_PROG, 0 } ;
-  char const *cenvp[1] = { 0 } ;
+  static char const *const cargv[2] = { FTRIGRD_PROG, 0 } ;
+  static char const *const cenvp[1] = { 0 } ;
   return skaclient_startf_b(&a->connection, &a->buffers, cargv[0], cargv, cenvp, SKACLIENT_OPTION_WAITPID, FTRIGR_BANNER1, FTRIGR_BANNER1_LEN, FTRIGR_BANNER2, FTRIGR_BANNER2_LEN, deadline, stamp) ;
 }
