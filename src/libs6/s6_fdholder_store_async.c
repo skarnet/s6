b11bacf27cd92528f4c2e157414c43b9cebd2b4d
Laurent Bercot <ska-skaware@skarnet.org>
2015-02-16 14:24:10 +0000

    One more spurious warning fix

--- src/libs6/s6_fdholder_store_async.c
+++ src/libs6/s6_fdholder_store_async.c
@@ -11,7 +11,7 @@ int s6_fdholder_store_async (s6_fdholder_t *a, int fd, char const *id, tain_t co
 {
   unsigned int idlen = str_len(id) ;
   char pack[2 + TAIN_PACK] = "S" ;
-  siovec_t v[2] = { { .s = pack, .len = 2 + TAIN_PACK }, { .s = id, .len = idlen + 1 } } ;
+  siovec_t v[2] = { { .s = pack, .len = 2 + TAIN_PACK }, { .s = (char *)id, .len = idlen + 1 } } ;
   unixmessage_v_t m = { .v = v, .vlen = 2, .fds = &fd, .nfds = 1 } ;
   if (idlen > S6_FDHOLDER_ID_SIZE) return (errno = ENAMETOOLONG, 0) ;
   tain_pack(pack + 1, limit) ;
