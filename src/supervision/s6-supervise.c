2190faba4c9dbff5408c3b16d567829b37740de2
AndCycle <andcycle-github@andcycle.idv.tw>
2016-12-05 23:41:00 +0800

    s6-supervise should also handle SIGINT as SIGTERM

--- src/supervision/s6-supervise.c
+++ src/supervision/s6-supervise.c
@@ -518,6 +518,7 @@ static inline void handle_signals (void)
           (*actions[state][V_CHLD])() ;
         }
         break ;
+      case SIGINT :
       case SIGTERM :
         (*actions[state][V_TERM])() ;
         break ;
@@ -573,6 +574,7 @@ int main (int argc, char const *const *argv)
     {
       sigset_t set ;
       sigemptyset(&set) ;
+      sigaddset(&set, SIGINT) ;
       sigaddset(&set, SIGTERM) ;
       sigaddset(&set, SIGHUP) ;
       sigaddset(&set, SIGQUIT) ;
