eed8f99badcf66b530bbdf245fa03fdc553380c6
Laurent Bercot <ska-skaware@skarnet.org>
2018-03-24 14:54:15 +0000

    Fix off-by-one in s6-svdt-clear

--- src/supervision/s6-svdt-clear.c
+++ src/supervision/s6-svdt-clear.c
@@ -8,7 +8,7 @@
 
 int main (int argc, char const *const *argv)
 {
-  if (argc < 1) dieusage() ;
+  if (argc < 2) dieusage() ;
   if (!s6_dtally_write(argv[1], 0, 0))
     strerr_diefu2sys(111, "clear death tally for service ", argv[1]) ;
   return 0 ;
