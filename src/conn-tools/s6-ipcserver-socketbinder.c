a9ac71ac2a874760466f673732610c2cc186797a
Laurent Bercot <ska-skaware@skarnet.org>
2018-04-11 14:58:04 +0000

    Add -B option to s6-ipcserver-socketbinder for blocking sockets

--- src/conn-tools/s6-ipcserver-socketbinder.c
+++ src/conn-tools/s6-ipcserver-socketbinder.c
@@ -9,7 +9,7 @@
 #include <skalibs/djbunix.h>
 #include <skalibs/webipc.h>
 
-#define USAGE "s6-ipcserver-socketbinder [ -d | -D ] [ -b backlog ] [ -M | -m ] [ -a perms ] path prog..."
+#define USAGE "s6-ipcserver-socketbinder [ -d | -D ] [ -b backlog ] [ -M | -m ] [ -a perms ] [ -B ] path prog..."
 #define dieusage() strerr_dieusage(100, USAGE)
 
 int main (int argc, char const *const *argv, char const *const *envp)
@@ -17,13 +17,14 @@ int main (int argc, char const *const *argv, char const *const *envp)
   unsigned int backlog = SOMAXCONN ;
   int flagreuse = 1 ;
   int flagdgram = 0 ;
+  int flagblocking = 0 ;
   unsigned int perms = 0777 ;
   PROG = "s6-ipcserver-socketbinder" ;
   {
     subgetopt_t l = SUBGETOPT_ZERO ;
     for (;;)
     {
-      int opt = subgetopt_r(argc, argv, "DdMmb:a:", &l) ;
+      int opt = subgetopt_r(argc, argv, "DdMmBb:a:", &l) ;
       if (opt == -1) break ;
       switch (opt)
       {
@@ -31,6 +32,7 @@ int main (int argc, char const *const *argv, char const *const *envp)
         case 'd' : flagreuse = 1 ; break ;
         case 'M' : flagdgram = 0 ; break ;
         case 'm' : flagdgram = 1 ; break ;
+        case 'B' : flagblocking = 1 ; break ;
         case 'b' : if (!uint0_scan(l.arg, &backlog)) dieusage() ; break ;
         case 'a' : if (!uint0_oscan(l.arg, &perms)) dieusage() ; break ;
         default : dieusage() ;
@@ -40,7 +42,9 @@ int main (int argc, char const *const *argv, char const *const *envp)
   }
   if (argc < 2) dieusage() ;
   close(0) ;
-  if (flagdgram ? ipc_datagram() : ipc_stream()) strerr_diefu1sys(111, "create socket") ;
+  if (flagdgram ? ipc_datagram_internal(flagblocking ? 0 : DJBUNIX_FLAG_NB) : ipc_stream_internal(flagblocking ? 0 : DJBUNIX_FLAG_NB))
+    strerr_diefu1sys(111, "create socket") ;
+
   {
     mode_t m = umask(~perms & 0777) ;
     if ((flagreuse ? ipc_bind_reuse(0, argv[0]) : ipc_bind(0, argv[0])) < 0)
