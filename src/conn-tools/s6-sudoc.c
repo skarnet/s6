ea2b67a78ba61808d9e0c24f947730cb8fead242
Laurent Bercot <ska-skaware@skarnet.org>
2019-02-01 08:56:48 +0000

    Fix s6-sudoc error message when it fails to connect

--- src/conn-tools/s6-sudoc.c
+++ src/conn-tools/s6-sudoc.c
@@ -60,7 +60,7 @@ int main (int argc, char const *const *argv, char const *const *envp)
     char tmp[S6_SUDO_BANNERB_LEN] ;
     r = buffer_timed_get_g(&b6, tmp, S6_SUDO_BANNERB_LEN, &deadline) ;
     if (!r)
-      strerr_dief1x(111, "connect to the s6-sudod server - check that you have appropriate permissions") ;
+      strerr_diefu1x(111, "connect to the s6-sudod server - check that you have appropriate permissions") ;
     if (r < S6_SUDO_BANNERB_LEN)
       strerr_diefu1sys(111, "read banner from s6-sudod") ;
     if (strncmp(tmp, S6_SUDO_BANNERB, S6_SUDO_BANNERB_LEN))
