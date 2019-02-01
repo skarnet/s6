d4eb8bc81bd3af8d9331e35cad5e305407ee4c3f
Laurent Bercot <ska-skaware@skarnet.org>
2019-02-01 00:30:07 +0000

    The ftrig1 must be read nonblocking by s6-ftrigrd, duh!
     GOD WHY AM I SUCH AN IDIOT

--- src/libs6/ftrig1_make.c
+++ src/libs6/ftrig1_make.c
@@ -29,6 +29,7 @@ int ftrig1_make (ftrig1_t *f, char const *path)
   memcpy(tmp + pathlen + FTRIG1_PREFIXLEN + 28, ":XXXXXX", 8) ;
   ff.fd = mkptemp(tmp) ;
   if (ff.fd == -1) return 0 ;
+  if (ndelay_on(ff.fd) == -1) goto err1 ;
   ff.fdw = open_write(tmp) ;
   if (ff.fdw == -1) goto err1 ;
   if (!stralloc_ready(&ff.name, pathlen + FTRIG1_PREFIXLEN + 36)) goto err2 ;
