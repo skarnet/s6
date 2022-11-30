aa62441bff80596baa513956c975eee97605212c
Laurent Bercot <ska-skaware@skarnet.org>
2022-11-30 07:52:55 +0000

    s6-svc: typo fix
    Signed-off-by: Laurent Bercot <ska@appnovation.com>

--- src/supervision/s6-svc.c
+++ src/supervision/s6-svc.c
@@ -68,7 +68,7 @@ int main (int argc, char const *const *argv)
     argc -= l.ind ; argv += l.ind ;
   }
   if (!argc) dieusage() ;
-  if (argc > 1) strerr_warn1x("ignoring extra arguments") ;
+  if (argc > 1) strerr_warnw1x("ignoring extra arguments") ;
 
   if (updown[1] == 'U' || updown[1] == 'R')
   {
