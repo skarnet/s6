1d92d42d2c3a7575cff3fdf0310ffc97d5038bd0
Laurent Bercot <ska-skaware@skarnet.org>
2024-10-04 12:09:53 +0000

    Add flag-newpidns to the servicedir file list
    Signed-off-by: Laurent Bercot <ska@appnovation.com>

--- src/libs6/s6_servicedir_file_list.c
+++ src/libs6/s6_servicedir_file_list.c
@@ -12,6 +12,7 @@ static s6_servicedir_desc const s6_servicedir_file_list_[] =
   { .name = "timeout-finish", .type = S6_FILETYPE_UINT, .options = 0 },
   { .name = "max-death-tally", .type = S6_FILETYPE_UINT, .options = 0 },
   { .name = "down-signal", .type = S6_FILETYPE_NORMAL, .options = 0 },
+  { .name = "flag-newpidns", .type = S6_FILETYPE_NORMAL, .options = 0 },
   { .name = "template", .type = S6_FILETYPE_DIR, .options = 0 },
   { .name = "data", .type = S6_FILETYPE_DIR, .options = 0 },
   { .name = "env", .type = S6_FILETYPE_DIR, .options = 0 },
