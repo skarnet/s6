96cd0d966a8bc0daad9ec3e18f00e69623f1b5db
Laurent Bercot <ska-skaware@skarnet.org>
2026-01-20 08:33:43 +0000

    Invoke s6-ftrigrd with a full environment
    That helps when LD_LIBRARY_PATH is necessary.

--- src/libs6/ftrigr_startf.c
+++ src/libs6/ftrigr_startf.c
@@ -1,11 +1,12 @@
 /* ISC license. */
 
+#include <skalibs/posixplz.h>
 #include <skalibs/textclient.h>
+
 #include <s6/ftrigr.h>
 
 int ftrigr_startf (ftrigr_t *a, tain const *deadline, tain *stamp)
 {
   static char const *const cargv[2] = { FTRIGRD_PROG, 0 } ;
-  static char const *const cenvp[1] = { 0 } ;
-  return textclient_startf(&a->connection, cargv, cenvp, TEXTCLIENT_OPTION_WAITPID, FTRIGR_BANNER1, FTRIGR_BANNER1_LEN, FTRIGR_BANNER2, FTRIGR_BANNER2_LEN, deadline, stamp) ;
+  return textclient_startf(&a->connection, cargv, (char const *const *)environ, TEXTCLIENT_OPTION_WAITPID, FTRIGR_BANNER1, FTRIGR_BANNER1_LEN, FTRIGR_BANNER2, FTRIGR_BANNER2_LEN, deadline, stamp) ;
 }
